c4b0173f13c5cb7b79785588f160b0301460f278
Konstantin Nochevny <kosnobiz@gmail.com>
2024-02-06 20:57:04 +0200

    Build a show-time component

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,19 @@
-<script setup>
-
-</script>
-
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-
   </header>
 
   <main>
+    <ShowTime></ShowTime>
   </main>
+
+  <footer>im the footer</footer>
 </template>
 
+<script setup>
+import ShowTime from './components/ShowTime.vue'
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-}
 </style>
---
+++ src/components/ShowTime.vue
@@ -0,0 +1,68 @@
+<template>
+    <div @click="toggleBgc" class="time-container" :class="{ dark: isDark }">
+        <p class="icon">{{ currentSeasonIcon }}</p>
+        <p>{{ formattedCurrentTime }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            isDark: false,
+            currentTime: new Date(),
+        }
+    },
+    created() {
+        this.interval = setInterval(() => {
+            this.currentTime = new Date()
+        }, 1000)
+    },
+    beforeDestroy() {
+        clearInterval(this.interval)
+    },
+    computed: {
+        formattedCurrentTime() {
+            return this.currentTime.toLocaleTimeString()
+        },
+        currentSeasonIcon() {
+            const month = this.currentTime.getMonth()
+            if (month >= 2 && month <= 4) {
+                return '🌸'
+            } else if (month >= 5 && month <= 7) {
+                return '☀️'
+            } else if (month >= 8 && month <= 10) {
+                return '🍂'
+            } else {
+                return '❄️'
+            }
+        }
+    },
+    methods: {
+        toggleBgc() {
+            this.isDark = !this.isDark
+        }
+    }
+}
+</script>
+
+<style scoped>
+div {
+    display: flex;
+    flex-flow: column;
+    align-items: center;
+    border: 2px solid blue;
+}
+
+.icon {
+    font-size: 2rem;
+}
+
+.dark {
+    background-color: darkgray;
+
+    p {
+        color: blue;
+    }
+}
+</style>
